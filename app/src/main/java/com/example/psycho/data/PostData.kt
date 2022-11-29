39b6e8085c9dc12d9508979b6b382eae99d539c6
Lemon-prog123 <2000013085@stu.pku.edu.cn>
2022-11-29 12:00:48 +0800

    简单修改PostData类

--- app/src/main/java/com/example/psycho/data/PostData.kt
+++ app/src/main/java/com/example/psycho/data/PostData.kt
@@ -1,4 +1,4 @@
 package com.example.psycho.data
 
-data class BaseData(var errCode:Int=10001,var errMsg:String="None")
+data class BaseData(var errCode:Int=10001,var errMsg:String="None",var id:Int=100)
 data class PostData(  var  status: String="fail",var  data:BaseData)
