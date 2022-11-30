6a4e2b738b8ed4b5f9cc75f691356fdff1a06267
lemon-prog123 <2000013085@stu.pku.edu.cn>
2022-11-30 19:54:06 +0800

    修复小BUG

---
+++ app/src/main/java/com/example/psycho/data/MyDatabaseHelper.kt
@@ -0,0 +1,51 @@
+package com.example.psycho.data
+import android.database.sqlite.SQLiteDatabase
+import android.content.Context
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+
+class MyDatabaseHelper (var context: Context, name: String, version: Int):SQLiteOpenHelper(context, name, null, version)
+{
+    public var createUser="create table User(" +
+            "uid integer primary key autoincrement," +
+            "weight real," +
+            "height real,"+
+            "birthday text,"+
+            "name text," +
+            "state int,"+
+            "gender int,"+
+            "avoidance int,"+
+            "login int,"+
+            "menu text)"
+
+    override fun onCreate(db: SQLiteDatabase?) {
+//        下面这个todo 如果不注释掉的话就会报错。
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db?.execSQL(createUser)
+        Toast.makeText(context,"Create Success",Toast.LENGTH_LONG).show()
+    }
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db?.execSQL("drop table if exists User")
+        onCreate(db)
+    }
+}
+
+class MyFoodHelper (var context: Context, name: String, version: Int):SQLiteOpenHelper(context, name, null, version)
+{
+    public var createFood="create table Food (" +
+            "id integer primary key autoincrement," +
+            "food json)"
+
+    override fun onCreate(db: SQLiteDatabase?) {
+//        下面这个todo 如果不注释掉的话就会报错。
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db?.execSQL(createFood)
+        Toast.makeText(context,"Create Success",Toast.LENGTH_LONG).show()
+    }
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+//        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db?.execSQL("drop table if exists Food")
+        onCreate(db)
+    }
+}
